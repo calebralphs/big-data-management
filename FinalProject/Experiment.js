2878b443637852ae5358602ca9abb4d2e0158198
Jackson Powell <jjpowell@wpi.edu>
2020-12-06 19:18:34 -0500

    Added outline for adding queries to experiment on

--- FinalProject/Experiment.js
+++ FinalProject/Experiment.js
@@ -2,3 +2,14 @@
 // Load HideAllIndexes in between queries to reset which indexes are available to use and unhide the ones that should be used
 // Run the same query before and after unhiding the index to get the comparison
 // Put the actual queries in a separate file reduce redundancy. Not sure if collection name can be parameterized.
+load("HideAllIndexes.js");
+load("Queries.js");
+const databases = ["people1","people10","people50"];
+for(let i = 0; i < 3; i++){
+    print()
+    print("-----------------------Experiment for database " + databases[i] + "-----------------------");
+    hideAllIndexes(databases[i]);
+
+    // Call a function in Queries.js to run all of the queries for a database. Make each query handle hiding and unhiding indexes.
+    runAllQueries(databases[i]);
+}
